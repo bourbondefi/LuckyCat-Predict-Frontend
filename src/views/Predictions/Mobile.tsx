8d819f5cae79133bd8d435737ca33bba7a157240
bourbondefi <80276717+bourbondefi@users.noreply.github.com>
2022-12-15 00:45:28 -0500

    Update Mobile.tsx

--- src/views/Predictions/Mobile.tsx
+++ src/views/Predictions/Mobile.tsx
@@ -39,7 +39,7 @@ const View = styled.div<{ isVisible: boolean }>`
 
 const PowerLinkStyle = styled.div`
   position: absolute;
-  right: 128px;
+  right: 118px;
   top: 24px;
 `
 
@@ -76,6 +76,11 @@ const Mobile: React.FC = () => {
     <StyledMobile>
       <Box height="100%" overflow="hidden" position="relative">
         <View isVisible={view === PageView.POSITIONS}>
+          <PowerLinkStyle>
+          <Button width="75%" className="mobile-button" onClick={handleButtonClick}>
+          End Round
+          </Button>
+          </PowerLinkStyle>
           <Flex alignItems="center" height="100%">
             {status === PredictionStatus.ERROR && <ErrorNotification />}
             {status === PredictionStatus.PAUSED && <PauseNotification />}
@@ -95,11 +100,6 @@ const Mobile: React.FC = () => {
         </View>
       </Box>
       <MobileMenu />
-      <PowerLinkStyle>
-        <Button width="75%" className="mobile-button" onClick={handleButtonClick}>
-          End Round
-        </Button>
-      </PowerLinkStyle>
     </StyledMobile>
   )
 }
