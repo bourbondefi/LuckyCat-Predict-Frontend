940076654c63749c543e88fb41dca90f611a204a
bourbondefi <80276717+bourbondefi@users.noreply.github.com>
2022-12-16 05:22:34 -0500

    Update SettingsModal.tsx

--- src/components/Menu/GlobalSettings/SettingsModal.tsx
+++ src/components/Menu/GlobalSettings/SettingsModal.tsx
@@ -76,6 +76,69 @@ const SettingsModal: React.FC<InjectedModalProps> = ({ onDismiss }) => {
           </Flex>
           <GasSettings />
         </Flex>
+        <Flex pt="24px" flexDirection="column" borderTop={`1px ${theme.colors.cardBorder} solid`}>
+           <Text bold textTransform="uppercase" fontSize="12px" color="secondary" mb="24px">
+             {t('Swaps & Liquidity')}
+           </Text>
+           <TransactionSettings />
+         </Flex>
+         <Flex justifyContent="space-between" alignItems="center" mb="24px">
+           <Flex alignItems="center">
+             <Text>{t('Expert Mode')}</Text>
+             <QuestionHelper
+               text={t('Bypasses confirmation modals and allows high slippage trades. Use at your own risk.')}
+               placement="top-start"
+               ml="4px"
+             />
+           </Flex>
+           <Toggle id="toggle-expert-mode-button" scale="md" checked={expertMode} onChange={handleExpertModeToggle} />
+         </Flex>
+         <Flex justifyContent="space-between" alignItems="center" mb="24px">
+           <Flex alignItems="center">
+             <Text>{t('Disable Multihops')}</Text>
+             <QuestionHelper text={t('Restricts swaps to direct pairs only.')} placement="top-start" ml="4px" />
+           </Flex>
+           <Toggle
+             id="toggle-disable-multihop-button"
+             checked={singleHopOnly}
+             scale="md"
+             onChange={() => {
+               setSingleHopOnly(!singleHopOnly)
+             }}
+           />
+         </Flex>
+         <Flex justifyContent="space-between" alignItems="center" mb="24px">
+           <Flex alignItems="center">
+             <Text>{t('Subgraph Health Indicator')}</Text>
+             <QuestionHelper
+               text={t(
+                 'Turn on NFT market subgraph health indicator all the time. Default is to show the indicator only when the network is delayed',
+               )}
+               placement="top-start"
+               ml="4px"
+             />
+           </Flex>
+           <Toggle
+             id="toggle-subgraph-health-button"
+             checked={subgraphHealth}
+             scale="md"
+             onChange={() => {
+               setSubgraphHealth(!subgraphHealth)
+             }}
+           />
+         </Flex>
+         <Flex justifyContent="space-between" alignItems="center">
+           <Flex alignItems="center">
+             <Text>{t('Flippy sounds')}</Text>
+             <QuestionHelper
+               text={t('Fun sounds to make a truly immersive pancake-flipping trading experience')}
+               placement="top-start"
+               ml="4px"
+             />
+           </Flex>
+           <PancakeToggle checked={audioPlay} onChange={toggleSetAudioMode} scale="md" />
+         </Flex>
+
       </ScrollableContainer>
     </Modal>
   )
