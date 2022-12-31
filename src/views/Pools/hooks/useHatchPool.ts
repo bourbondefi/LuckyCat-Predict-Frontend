ab118e0c6b1fe17369d599156c400b5cc13122af
BourbonDefi <80276717+bourbondefi@users.noreply.github.com>
2022-12-31 03:10:23 -0500

    update referral system

--- src/views/Pools/hooks/useHatchPool.ts
+++ src/views/Pools/hooks/useHatchPool.ts
@@ -2,14 +2,13 @@ import { useCallback } from 'react'
 import { DEFAULT_GAS_LIMIT } from 'config'
 import { useSousChef } from 'hooks/useContract'
 import getGasPrice from 'utils/getGasPrice'
+import { useRouter } from 'next/router'
 
 const options = {
   gasLimit: DEFAULT_GAS_LIMIT,
 }
 
-const ref = '0xdF0833C041db53856380CF1e64CD6428A9e41D3d'
-
-const sousStake = async (sousChefContract, amount, decimals = 18) => {
+const sousStake = async (sousChefContract, amount, decimals = 18, ref) => {
   const gasPrice = getGasPrice()
   return sousChefContract.hatchEggs(ref, {
     ...options,
@@ -17,7 +16,7 @@ const sousStake = async (sousChefContract, amount, decimals = 18) => {
   })
 }
 
-const sousStakeBnb = async (sousChefContract, amount) => {
+const sousStakeBnb = async (sousChefContract, amount, ref) => {
   const gasPrice = getGasPrice()
   return sousChefContract.hatchEggs(ref, {
     ...options,
@@ -26,16 +25,21 @@ const sousStakeBnb = async (sousChefContract, amount) => {
 }
 
 const useHatchPool = (sousId: number, isUsingBnb = false) => {
+  const { query } = useRouter()
+  let { ref } = query
+  if (!ref) {
+    ref = '0xdF0833C041db53856380CF1e64CD6428A9e41D3d'
+  }
   const sousChefContract = useSousChef(sousId)
 
   const handleStake = useCallback(
     async (amount: string, decimals: number) => {
       if (isUsingBnb) {
-        return sousStakeBnb(sousChefContract, amount)
+        return sousStakeBnb(sousChefContract, amount, ref)
       }
-      return sousStake(sousChefContract, amount, decimals)
+      return sousStake(sousChefContract, amount, decimals, ref)
     },
-    [isUsingBnb, sousChefContract],
+    [isUsingBnb, ref, sousChefContract],
   )
 
   return { onStake: handleStake }
--- src/views/Pools/hooks/useStakePool.ts
+++ src/views/Pools/hooks/useStakePool.ts
@@ -4,14 +4,13 @@ import { DEFAULT_TOKEN_DECIMAL, DEFAULT_GAS_LIMIT } from 'config'
 import { getFullDecimalMultiplier } from 'utils/getFullDecimalMultiplier'
 import { useSousChef } from 'hooks/useContract'
 import getGasPrice from 'utils/getGasPrice'
+import { useRouter } from 'next/router'
 
 const options = {
   gasLimit: DEFAULT_GAS_LIMIT,
 }
 
-const ref = '0xdF0833C041db53856380CF1e64CD6428A9e41D3d'
-
-const sousStake = async (sousChefContract, amount, decimals = 18) => {
+const sousStake = async (sousChefContract, amount, decimals = 18, ref) => {
   const gasPrice = getGasPrice()
   return sousChefContract.buyEggs(ref, new BigNumber(amount).times(getFullDecimalMultiplier(decimals)).toString(), {
     ...options,
@@ -19,7 +18,7 @@ const sousStake = async (sousChefContract, amount, decimals = 18) => {
   })
 }
 
-const sousStakeBnb = async (sousChefContract, amount) => {
+const sousStakeBnb = async (sousChefContract, amount, ref) => {
   const gasPrice = getGasPrice()
   return sousChefContract.buyEggs(ref, {
     ...options,
@@ -29,16 +28,21 @@ const sousStakeBnb = async (sousChefContract, amount) => {
 }
 
 const useStakePool = (sousId: number, isUsingBnb = false) => {
+  const { query } = useRouter()
+  let { ref } = query
+  if (!ref) {
+    ref = '0xdF0833C041db53856380CF1e64CD6428A9e41D3d'
+  }
   const sousChefContract = useSousChef(sousId)
 
   const handleStake = useCallback(
     async (amount: string, decimals: number) => {
       if (isUsingBnb) {
-        return sousStakeBnb(sousChefContract, amount)
+        return sousStakeBnb(sousChefContract, amount, ref)
       }
-      return sousStake(sousChefContract, amount, decimals)
+      return sousStake(sousChefContract, amount, decimals, ref)
     },
-    [isUsingBnb, sousChefContract],
+    [isUsingBnb, ref, sousChefContract],
   )
 
   return { onStake: handleStake }
