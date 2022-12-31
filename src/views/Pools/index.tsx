dd44086c1e50bd57b6c8b45e2e254b1250775698
BourbonDefi <80276717+bourbondefi@users.noreply.github.com>
2022-12-30 20:08:24 -0500

    Update index.tsx

--- src/views/Pools/index.tsx
+++ src/views/Pools/index.tsx
@@ -317,16 +317,7 @@ const Pools: React.FC = () => {
             </LabelWrapper>
           </FilterContainer>
         </PoolControls>
-        {showFinishedPools && (
-          <FinishedTextContainer>
-            <Text fontSize={['16px', null, '20px']} color="failure" pr="4px">
-              {t('Looking for v1 CAKE syrup pools?')}
-            </Text>
-            <FinishedTextLink href="/migration" fontSize={['16px', null, '20px']} color="failure">
-              {t('Go to migration page')}.
-            </FinishedTextLink>
-          </FinishedTextContainer>
-        )}
+        {showFinishedPools}
         {account && !userDataLoaded && stakedOnly && (
           <Flex justifyContent="center" mb="4px">
             <Loading />
